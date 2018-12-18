492104fa0e4097a6f1b0edc8ecb3e3ae7d6a1241
qingmingzi <matrix3345@hotmail.com>
2018-12-18 13:34:52 +0800

    增加TSDB入库块高度、时间信息

--- blcokStatusMonitor/Program.cs
+++ blcokStatusMonitor/Program.cs
@@ -12,13 +12,16 @@ using Microsoft.Extensions.Configuration;
 using blcokStatusMonitor.lib;
 using System.Threading;
 
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
 namespace blcokStatusMonitor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("监控开始");
+            Console.WriteLine("监控开始");         
 
             var builder = new ConfigurationBuilder();
             builder.AddJsonFile("keySetting.json");
@@ -33,14 +36,21 @@ namespace blcokStatusMonitor
             string mongodbDatabase_testnet = configuration["mongodbDatabase_testnet"];
             string mongodbConnStr_mainnet = configuration["mongodbConnStr_mainnet"];
             string mongodbDatabase_mainnet = configuration["mongodbDatabase_mainnet"];
+            string TSDB_URL = configuration["TSDB_URL"];
 
             bool isDailySend_testnet = false;
             string blockNotInHour_testnet = string.Empty;
             bool isDailySend_mainnet = false;
             string blockNotInHour_mainnet = string.Empty;
             Aliyun_SMS aliSMS = new Aliyun_SMS(accessKeyId, accessKeySecret);
+            Aliyun_TSDB aliyun_TSDB = new Aliyun_TSDB(TSDB_URL);
+
+
             while (true)
             {
+                //block时间信息入库
+                insertBlockTime2TSDB(mongodbConnStr_testnet, mongodbDatabase_testnet, aliyun_TSDB,"testnet");
+
                 //监控测试网
                 exeNotify("测试网",mongodbConnStr_testnet, mongodbDatabase_testnet, dailyTime,ref isDailySend_testnet,ref blockNotInHour_testnet, aliSMS, phoneNumbers);
 
@@ -49,10 +59,41 @@ namespace blcokStatusMonitor
 
                 //Console.ReadKey();
 
-                Thread.Sleep(15*1000);
+                Thread.Sleep(500);
             }
 
 
+        }
+
+        private static void insertBlockTime2TSDB(string mongodbConnStr, string mongodbDatabase, Aliyun_TSDB aliyun_TSDB,string netType) {
+            long maxBloclHeight = -1;
+            try
+            {
+                var maxBlock = aliyun_TSDB.TSDB_query(DateTime.Today.AddDays(-1), DateTime.Today.AddDays(1), "max", "neo.block.height.done", JObject.Parse("{'net': '" + netType + "'}"), "2d-max");
+                maxBloclHeight = (long)decimal.Parse(maxBlock[0]["dps"].First.ToString().Split(":")[1]);
+            }
+            catch { }
+
+            mongodbHelper mh = new mongodbHelper();
+            JArray JA = mh.GetData(mongodbConnStr, mongodbDatabase, "system_counter", "{}");
+            long maxBlockHeight_DB = long.Parse(JA.Children().Where(n => n["counter"].ToString() == "block").Select(n => (string)n["lastBlockindex"]).ToArray()[0]);
+
+            long doBlockHeight = maxBloclHeight + 1;
+            if (doBlockHeight <= maxBlockHeight_DB)
+            {
+                JArray blockJA = mh.GetData(mongodbConnStr, mongodbDatabase, "block", "{index:" + doBlockHeight + "}");
+                var lastBlockTime = (long)blockJA[0]["time"];
+                try
+                {
+                    var a = aliyun_TSDB.TSDB_put_single(new Aliyun_TSDB.TSDB_Data("neo.block.height", lastBlockTime, doBlockHeight, JObject.Parse("{'net': '" + netType + "'}")));
+                    var b = aliyun_TSDB.TSDB_put_single(new Aliyun_TSDB.TSDB_Data("neo.block.height.done", DateTime.Now, doBlockHeight, JObject.Parse("{'net': '" + netType + "'}")));
+                    Console.WriteLine("写入高度：" + doBlockHeight + "块时间到TSDB");
+                } 
+                catch {
+                    Console.WriteLine("写入TSDB失败，重试！");
+                }
+                    
+            }
         }
 
         private static void exeNotify(string netType,string mongodbConnStr,string mongodbDatabase,string dailyTime,ref bool isDailySend,ref string blockNotInHour, Aliyun_SMS aliSMS,string phoneNumbers)
@@ -90,6 +131,7 @@ namespace blcokStatusMonitor
 
             }
 
+            Console.WriteLine(netType + "检查无异常");
         }
     }
 }
---
+++ blcokStatusMonitor/lib/aliyun_TSDB.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace blcokStatusMonitor.lib
+{
+    public class Aliyun_TSDB
+    {
+        public string TSDB_URL = string.Empty;
+        httpHelper http = new httpHelper();
+
+        public Aliyun_TSDB(string URL) {
+            TSDB_URL = URL;
+        }
+
+        public class TSDB_Data {
+            public string Metric { get; set; }
+            public long Timestamp { get; set; }
+            public object Value { get; set; }
+            public JObject Tags { get; set; }
+
+            public TSDB_Data(string metric, DateTime time, object value, JObject tags)
+            {
+                Metric = metric;
+
+                Timestamp = getTimeStamp(time);
+
+                Value = value;
+                Tags = tags;
+            }
+
+            public TSDB_Data(string metric, long timeStamp, object value, JObject tags)
+            {
+                Metric = metric;
+
+                Timestamp = timeStamp;
+
+                Value = value;
+                Tags = tags;
+            }
+        }
+
+        public JObject TSDB_put_single(TSDB_Data tSDB_Data)
+        {
+            var inputParam = "[" + JsonConvert.SerializeObject(tSDB_Data) + "]";
+            return JObject.Parse(http.Post(TSDB_URL + "/api/put?summary", inputParam, Encoding.UTF8, 1));
+        }
+
+        public JArray TSDB_query(DateTime startTime,DateTime endTime,string aggregator, string metric, JObject tags,string downsample)
+        {
+            var inputParamJ = JObject.Parse(@"
+            {
+                'start': [0],
+                'end': [1],
+                'queries': [
+                    {
+                        'aggregator': '[2]',
+                        'metric': '[3]',
+                        'tags': [4],
+                        'downsample':'[5]'
+                    }
+                ]
+            }");
+            inputParamJ["start"] = getTimeStamp(startTime);
+            inputParamJ["end"] = getTimeStamp(endTime);
+            inputParamJ["queries"][0]["aggregator"] = aggregator;
+            inputParamJ["queries"][0]["metric"] = metric;
+            inputParamJ["queries"][0]["tags"] = tags;
+            inputParamJ["queries"][0]["downsample"] = downsample;
+
+            //var inputParam = @"
+            //{
+            //    'start': [0],
+            //    'end': [1],
+            //    'queries': [
+            //        {
+            //            'aggregator': 'count',
+            //            'metric': '[2]',
+            //            'tags': [3],
+            //            'downsample':'[4]'
+            //        }
+            //    ]
+            //}".Replace("[0]",getTimeStamp(startTime).ToString()).Replace("[1]",getTimeStamp(endTime).ToString()).Replace("[2]",metric).Replace("[3]",JsonConvert.SerializeObject(tags)).Replace("[4]",downsample).Replace("'","\"");
+
+            return JArray.Parse(http.Post(TSDB_URL + "/api/query", inputParamJ.ToString(), Encoding.UTF8, 1));
+        }
+
+        static private long getTimeStamp(DateTime dateTime)
+        {
+            DateTime startTime = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 8, 0, 0), TimeZoneInfo.Local); // 当地时区
+            return (long)(dateTime - startTime).TotalMilliseconds;
+        }
+    }
+}
